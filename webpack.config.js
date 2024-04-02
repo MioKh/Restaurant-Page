4edfbef9720c2ff239423fd64c8fbb3ea1ea5cdd
Amro Khalfa <mioakiyamakh@gmail.com>
2024-04-02 23:30:21 +0300

    saving to work on another device

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: 'index.js',
+  entry: './src/index.js',
+  mode: 'development',
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'index_bundle.js',
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
-  plugins: [new HtmlWebpackPlugin()],
-  template: './src/index.html',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
 };
